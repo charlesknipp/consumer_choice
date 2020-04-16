b8f5f280c3baa9a7627b06aa17b43448ea1a0422
Charles Knipp <charlesknipp@Charless-MacBook-Pro.local>
2020-04-16 15:18:34 -0700

    working in n dimensions

--- lagrangian_dual.py
+++ lagrangian_dual.py
@@ -2,26 +2,33 @@ import math
 import time
 
 
-def budget(x,m,p):
-    if len(p) != len(x):
-        raise IndexError('dim(x) not equal to dim(p)')
+# calculate the Lagrange Dual for a consumer choice problem with two goods
+# and Cobb-Douglas preferences
+
+def budget(x,p,**kwargs):
+    if len(p) < len(x):
+        raise IndexError('more goods than prices')
 
     items = [p[i]*x[i] for i in range(len(x))]
-    return sum(items)
+    m = kwargs.get('m',None)
 
-def cobbDouglas(x,u,t):
-    if len(t) != len(x):
-        raise IndexError('dim(x) not equal to dim(t)')
+    if (m == None) & (len(p) == len(x)):
+        return sum(items)
 
-    items = [x[i]**t[i] for i in range(len(x))]
-    return math.prod(items)
+    return ((m-p[0]*x[0])/p[1])
 
-# let $f(x)$ be the objective constrained by both a sequesnce of inequalities
-# $g_i(x)$ and sequence of equlaity constraints $h_j(x)$ respectively indexed
-# at $i \in {1,...,m}$ and $j \in {1,...,l}$
-#
-# Kuhn Tucker States that the following conditions must hold.
-#   1) $g_i(x^*) \geq 0$ and $h_j(x^*) = 0$
-#   2) $\mu_i \geq 0$
-#   3) $\mu_i \cdot g_i(x*) = 0$
-#   4) $Df_x(x^*) + \sum{j=1}^{l} \lambda_j^* \cdot Dh_j(x^*) + \sum{i=1}^{m} \mu_i^* \cdot Dg_i(x^*) = 0$
+
+def utility(x,pp,**kwargs):
+    if len(pp) < len(x):
+        raise IndexError('more goods than preference parameters')
+
+    items = [x[i]**pp[i] for i in range(len(x))]
+    u = kwargs.get('u',None)
+
+    if (u == None) & (len(pp) == len(x)):
+        return math.prod(items)
+
+    return (u*x[0]**(-pp[0]))**(1/pp[1])
+
+
+# t = (prices,expenditures)?

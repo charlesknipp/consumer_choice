89552ca23a9d46d7e277788e6d194f70e00fad40
classicharlie <cknipp@asu.edu>
2020-04-17 21:39:41 -0700

    lagrangean updates

--- lagrangian_dual.py
+++ lagrangian_dual.py
@@ -5,30 +5,73 @@ import time
 # calculate the Lagrange Dual for a consumer choice problem with two goods
 # and Cobb-Douglas preferences
 
-def budget(x,p,**kwargs):
-    if len(p) < len(x):
-        raise IndexError('more goods than prices')
-
-    items = [p[i]*x[i] for i in range(len(x))]
-    m = kwargs.get('m',None)
-
-    if (m == None) & (len(p) == len(x)):
-        return sum(items)
-
-    return ((m-p[0]*x[0])/p[1])
+def budget(x,p):
+    return sum([x[i]*p[i] for i in range(len(x))])
 
 
-def utility(x,pp,**kwargs):
-    if len(pp) < len(x):
-        raise IndexError('more goods than preference parameters')
+def utility(x,pp):
+    return math.prod([x[i]**pp[i] for i in range(len(x))])
 
-    items = [x[i]**pp[i] for i in range(len(x))]
-    u = kwargs.get('u',None)
+pp = [.3,.7]
+m = 100
+p = [3,2]
+n = len(p)
+error = 3
+k = (10**(-error))
 
-    if (u == None) & (len(pp) == len(x)):
-        return math.prod(items)
+x = [(n**(-1))*m/p[i] for i in range(len(p))]
+u = utility(x,pp)
 
-    return (u*x[0]**(-pp[0]))**(1/pp[1])
+def findLambda(x1):
+    x2 = [i+k for i in x1]
+    xs = []
+
+    # duplicate bundle x, n times, perturbing an element each iteration
+
+    for i in range(n):
+        l = []
+        for j in range(n):
+            if i != j:
+                l.append(x1[j])
+            else:
+                l.append(x2[j])
+        xs.append(l)
+
+    return [((utility(xs[i],pp)-utility(x1,pp))/k)/p[i] for i in range(n)]
 
 
-# t = (prices,expenditures)?
+def adjust(lb,ub):
+    x = (lb + ub)/2
+    return x
+
+
+min_x = [0,0]
+max_x = [m/p[0],m/p[1]]
+
+its = findLambda(x)
+counter = 0
+
+while True:
+    if its[1] - its[0] < .00001:
+        max_x[1] = x[1]
+        min_x[0] = x[0]
+        x[1] = adjust(min_x[1],max_x[1])
+        x[0] = (m-p[1]*x[1])/p[0]
+    
+    elif its[0] - its[1] < .00001:
+        max_x[0] = x[0]
+        min_x[1] = x[1]
+        x[0] = adjust(min_x[0],max_x[0])
+        x[1] = (m-p[0]*x[0])/p[1]
+
+    else:
+        break
+
+    if counter == 100:
+        break
+
+    counter += 1
+    its = findLambda(x)
+
+print('bundle\t(%.3f,%.3f)' % (x[0],x[1]))
+print('lambda\t(%.5f,%.5f)' % (its[0],its[1]))

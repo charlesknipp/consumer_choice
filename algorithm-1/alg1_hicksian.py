3029b68db3a46e6da05d4a6df505ad9a6994bc8b
Charles Knipp <charlesknipp@charless-mbp.lan>
2020-05-19 14:42:29 -0700

    intersection bug fixes

---
+++ algorithm-1/alg1_hicksian.py
@@ -0,0 +1,141 @@
+import time
+import math
+
+
+# this version uses the linear combination of values along an equivalence class,
+# which are constrained to a sublattice valued budget set.
+
+def hicksianDemand(utility,preferences,prices):
+    '''
+    computes an optimal bundle given a convex objective and a linear constraint, 
+    in the context of consumer theory; this particular instance limits the user 
+    to a Cobb-Douglas utility function, but can easily be expanded to more 
+    general expressions.
+
+    parameters:
+        income (int): the amount of money that the  consumer is constrained to 
+        spending
+        preferences (array): an array of taste parameters that applies to 
+        utility
+        prices (array): an array containing the prices of good 1 and good 2
+
+    returns:
+        bundle (tuple): the optimal bundle x to maximize utility subject to a 
+        budget constraint
+    '''
+
+    start_time = time.time()
+
+    a = preferences[0]
+    b = preferences[1]
+    u = utility
+    p = prices
+
+    x_i = [u**(1/(a+b)),u**(1/(a+b))]
+    m = sum([x_i[i]*p[i] for i in range(2)])
+
+    print(m)
+
+    def U(x1,utl):
+        return (utl*x1**(-a))**(1/b)
+
+
+    def B(x1,inc):
+        return ((inc-p[0]*x1)/p[1])
+
+
+    def findIntersection(income):
+        '''
+        Calculates the intersection of two curves using a dynamic step size that 
+        finds a lower limit and divides that interval by 10 each time.
+
+        returns:
+            bounds (array): structured like (min,max), this array represents the 
+            points of intersection of the given curve
+        '''
+
+        def findMin(x,k):
+            min_f = abs(U(x[0],u) - B(x[0],income))
+            min_x = x[0]
+
+            for i in x:
+                diff = U(i,u) - B(i,income)
+                next_diff = U(i+k,u) - B(i+k,income)
+                if diff < min_f:
+                    min_f = abs(diff)
+                    min_x = i
+                    if abs(diff) <= abs(next_diff):
+                        break
+
+            return min_x
+
+
+        def findMax(x,k):
+            max_f = abs(U(x[-1],u) - B(x[-1],income))
+            max_x = x[-1]
+
+            for i in list(reversed(x)):
+                diff = U(i+k,u) - B(i+k,income)
+                prev_diff = U(i,u) - B(i,income)
+                if diff < max_f:
+                    max_f = abs(prev_diff)
+                    max_x = i
+                    if abs(diff) <= abs(prev_diff):
+                        break
+
+            return max_x
+
+
+        n = 3
+        x = range(1,math.floor(m/p[0])+1)
+
+        min_x = [findMin(x,1)]
+        max_x = [findMax(x,1)]
+
+        for i in range(1,n+1):
+            k = 10**(-i)
+            mnx = [round(min_x[-1]+j*(k),i) for j in range(0,10)]
+            mxx = [round(max_x[-1]+j*(k),i) for j in range(0,10)]
+
+            min_x.append(findMin(mnx,k))
+            max_x.append(findMax(mxx,k))
+
+        final_min = min_x[-1]
+        final_max = max_x[-1]
+
+        bds = [final_min,final_max]
+        return bds
+
+
+    def adjust(lower_bound,upper_bound):
+        x1 = (lower_bound+upper_bound)/2
+        x2 = U(x1,u)
+        bundle = [x1,x2]
+        budget = sum([bundle[i]*p[i] for i in range(2)])
+
+        return budget
+
+
+    bounds = findIntersection(m)
+
+    while True:
+        if bounds[1] - bounds[0] > .001:
+            m = adjust(bounds[0],bounds[1])
+        else:
+            break
+
+        bounds = findIntersection(m)
+
+    bundle = (bounds[0],B(bounds[0],m))
+    elapsed_time = time.time() - start_time
+
+    return [bundle,elapsed_time]
+
+
+v = hicksianDemand(24.03519395349548,[.3,.7],[3,2])
+bundle = v[0]
+time = v[1]
+
+
+print('bundle:\t(%.2f,%.2f)' % v[0])
+print('time:\t%.3f microseconds' % (v[1]*1000))

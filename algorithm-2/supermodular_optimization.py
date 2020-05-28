2c8302eaebb5716b580eab99f008addb2d12f81d
Charles Knipp <charlesknipp@charless-mbp.3500.dhcp.asu.edu>
2020-05-28 11:12:51 -0700

    hicksian intersection bug fix

--- algorithm-2/supermodular_optimization.py
+++ algorithm-2/supermodular_optimization.py
@@ -60,23 +60,25 @@ def hicksianCase(utility,preferences,prices):
                 next_diff = U(i+k,u) - B(i+k,income)
                 if diff < min_f:
                     min_f = abs(diff)
-                    min_x = i
+                    min_x = i-k
                     if abs(diff) <= abs(next_diff):
                         break
 
             return min_x
 
-
+        # only works if you set prev_diff as the base and return -2k instead of
+        # setting the base as diff like I did in Go (which works flawlessly)
+        
         def findMax(x,k):
             max_f = abs(U(x[-1],u) - B(x[-1],income))
             max_x = x[-1]
 
             for i in list(reversed(x)):
-                diff = U(i+k,u) - B(i+k,income)
-                prev_diff = U(i,u) - B(i,income)
+                diff = U(i,u) - B(i,income)
+                prev_diff = U(i-k,u) - B(i-k,income)
                 if diff < max_f:
                     max_f = abs(prev_diff)
-                    max_x = i
+                    max_x = i-2*k
                     if abs(diff) <= abs(prev_diff):
                         break
 
@@ -91,8 +93,8 @@ def hicksianCase(utility,preferences,prices):
 
         for i in range(1,n+1):
             k = 10**(-i)
-            mnx = [round(min_x[-1]+j*(k),i) for j in range(0,10)]
-            mxx = [round(max_x[-1]+j*(k),i) for j in range(0,10)]
+            mnx = [round(min_x[-1]+j*(k),i) for j in range(0,20)]
+            mxx = [round(max_x[-1]+j*(k),i) for j in range(0,20)]
 
             min_x.append(findMin(mnx,k))
             max_x.append(findMax(mxx,k))
@@ -102,7 +104,6 @@ def hicksianCase(utility,preferences,prices):
 
         bds = [final_min,final_max]
         return bds
-
 
     def adjust(lower_bound,upper_bound):
         x1 = (lower_bound+upper_bound)/2

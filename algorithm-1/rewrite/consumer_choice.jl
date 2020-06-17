43088ae8ffddd67e4bdc183c98a802e9499f4790
Charles Knipp <cknipp@asu.edu>
2020-06-17 11:50:43 -0700

    julia optimization

--- algorithm-1/rewrite/consumer_choice.jl
+++ algorithm-1/rewrite/consumer_choice.jl
@@ -1,19 +1,19 @@
 # rewrite in Julia because it is faster than Python and easier than Go
 
-function marshallianDemand(m::Number)
+function marshallianDemand(m::Number, p::Array, t::Array)
 
     # define indifference
     function I(utl::Number,x1::Number)
-        (utl*x1^(-.3))^(1/.7)
+        (utl*x1^(-t[1]))^(1/t[2])
     end
 
     # define affordability
     function B(x1::Number)
-        (m-x1*3)/2
+        (m-x1*p[1])/p[2]
     end
 
     # set initial affordable guess
-    u = (.5*(m/3))^(.3)*(.5*(m/2))^(.7)
+    u = (.5*(m/p[1]))^(t[1])*(.5*(m/p[2]))^(t[2])
 
     function findIntersection(utl::Float64)
         function f(x1::Float64)
@@ -22,33 +22,33 @@ function marshallianDemand(m::Number)
 
         # derive using the limit definition
         function derive(x1::Float64)
-            h = .00001
+            h = .0001
             return (f(x1+h)-f(x1))/h
         end
 
-        function newtonsMethod(x0::Float64,maxiter::Integer=50)
+        function newtonsMethod(x0::Float64,maxiter::Integer=30)
             for _ in 1:maxiter
                 yprime = derive(x0)
-                if abs(yprime) < 1e-10
+                if abs(yprime) < 1e-5
                     return x0
                 end
                 y = f(x0)
                 x1 = x0 - y/yprime
-                if abs(x1-x0) < 1e-8
+                if abs(x1-x0) < 1e-5
                     return x1
                 end
                 x0 = x1
             end
             error("Max iteration exceeded")
         end
-        return [newtonsMethod(.0001),newtonsMethod(m/3)]
+        return [newtonsMethod(.001),newtonsMethod(m/p[1])]
     end
 
     # adjust bounds
     function adjust(lb,ub)
         x1 = (lb+ub)/2
         x2 = B(x1)
-        return x1^(.3)*x2^(.7)
+        return x1^(t[1])*x2^(t[2])
     end
 
     bounds = findIntersection(u)
@@ -65,4 +65,4 @@ function marshallianDemand(m::Number)
     return (bounds[1],I(u,bounds[1]))
 end
 
-@time marshallianDemand(100)
+@time marshallianDemand(100,[3 2],[.3 .7])

8f75ce35ac1c38958d500738bbe753e4fb8b1d85
Charles Knipp <cknipp@asu.edu>
2020-06-18 07:26:35 -0700

    dual cases in julia

--- algorithm-1/rewrite/consumer_choice.jl
+++ algorithm-1/rewrite/consumer_choice.jl
@@ -65,4 +65,73 @@ function marshallianDemand(m::Number, p::Array, t::Array)
     return (bounds[1],I(u,bounds[1]))
 end
 
+
+function hicksianDemand(u::Number, p::Array, t::Array)
+
+    # define indifference
+    function I(x1::Number)
+        (u*x1^(-t[1]))^(1/t[2])
+    end
+
+    # define affordability
+    function B(inc::Float64,x1::Number)
+        (inc-x1*p[1])/p[2]
+    end
+
+    # set initial indifferent guess
+    x_i = [u^(1/(t[1]+t[2])),u^(1/(t[1]+t[2]))]
+    m = sum([x_i[i]*p[i] for i in 1:2])
+
+    function findIntersection(inc::Float64)
+        function f(x1::Float64)
+            I(x1) - B(inc,x1)
+        end
+
+        # derive using the limit definition
+        function derive(x1::Float64)
+            h = .0001
+            return (f(x1+h)-f(x1))/h
+        end
+
+        function newtonsMethod(x0::Float64,maxiter::Integer=30)
+            for _ in 1:maxiter
+                yprime = derive(x0)
+                if abs(yprime) < 1e-5
+                    return x0
+                end
+                y = f(x0)
+                x1 = x0 - y/yprime
+                if abs(x1-x0) < 1e-5
+                    return x1
+                end
+                x0 = x1
+            end
+            error("Max iteration exceeded")
+        end
+        return [newtonsMethod(.001),newtonsMethod(m/p[1])]
+    end
+
+    # adjust bounds
+    function adjust(lb,ub)
+        x1 = (lb+ub)/2
+        x2 = I(x1)
+        return x1*(p[1]) + x2*(p[2])
+    end
+
+    bounds = findIntersection(m)
+
+    # begin recursion
+    while true
+        if abs(bounds[2]-bounds[1]) > .0001
+            m = adjust(bounds[1],bounds[2])
+        else
+            break
+        end
+        bounds = findIntersection(m)
+    end
+    return (bounds[1],B(m,bounds[1]))
+end
+
+# benchmarking
 @time marshallianDemand(100,[3 2],[.3 .7])
+@time hicksianDemand(24.03519395349548,[3 2],[.3 .7])

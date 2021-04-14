9316e52747f6b5cdd847cfbdb8fa1410256463db
classicharlie <cknipp@asu.edu>
2021-04-13 19:21:07 -0700

    added projective gradient ascent

--- drafts/draft-4.jl
+++ drafts/draft-4.jl
@@ -1,28 +1,30 @@
 using Base.Iterators
+using Plots
+using LinearAlgebra
 
 # just use Cobb-Douglas because it is supermodular in c on C = {c|c∈Rⁿ,α≥0}
-α = [1.0/3.0,1.0/3.0,1.0/3.0]
+α = [.5,.25,.25]
 f(x) = reduce(*,x.^α)
 lagrange(x,λ) = f(x) + λ*(m-reduce(+,p.*x))
 
 # the lagrangian is in terms of a vector, should probably rethink how my data is
 # structured
 
-function constraintSet(p::AbstractArray,t::Float64,δ::Int=2)
+function constraintSet(p::Array,t::Float64,equality::Bool=false,δ::Int=2)
+    feasible(x)  = sum(p.*x)-t ≤ 0 ? true : false
+    efficient(x) = abs(sum(p.*x)-t) ≤ exp10(-δ) ? true : false
 
-    feasible(x) = sum(p.*x)-t ≤ 0 ? true : false
-    rnd(p_i) = round(t/p_i,digits=δ)
-    ϵ = 10.0^(-δ)
+    rnd(pᵢ) = round(t/pᵢ,digits=δ)
+    ϵ = exp10(-δ)
 
-    max_vals = [rnd(p_i) ≤ t/p_i ? rnd(p_i) : rnd(p_i)-ϵ for p_i in p]
-    space = product((0.0:ϵ:max for max in max_vals)...)
-    space = vec(collect.(space))
+    max_vals = [rnd(pᵢ) ≤ t/pᵢ ? rnd(pᵢ) : rnd(pᵢ)-ϵ for pᵢ in p]
+    space = vec(collect.(product((0.0:ϵ:max for max in max_vals)...)))
 
-    return filter(feasible,space)
+    return equality == true ? filter(efficient,space) : filter(feasible,space)
 end
 
+
 function lineSearch(Γ)
-    
     global x_optimal
     x_optimal = Γ[1]
     
@@ -37,5 +39,58 @@ function lineSearch(Γ)
     return x_optimal
 end
 
-Γ = constraintSet([2.0,2.0,2.0],3.0)
-lineSearch(Γ)
+# Γ1 = constraintSet([2.0,2.0,2.0],3.0,false)
+# Γ2 = constraintSet([2.0,2.0,2.0],3.0,true)
+
+# @time lineSearch(Γ1)
+
+# derive using the limit definition
+function subgradient(x::Vector,step::Float64)
+    ∇f = []
+
+    for i = 1:length(x)
+        x_prime = copy(x)
+        x_prime[i] += step
+        push!(∇f, (f(x_prime)-f(x))/step)
+    end
+
+    return ∇f
+end
+
+function gradientAscent(p,t,δ,max_iters=1000)
+    n = length(p)
+    ϵ = exp10(-δ)
+
+    global xₖ
+    x₀ = [t/(n*pᵢ) for pᵢ in p]
+    xₖ = x₀
+
+    global α
+    αₖ = 1
+
+    ∇f(x) = subgradient(x,.00000001)
+    proj  = Matrix(1.0I,n,n) - p*inv(p'*p)*p'
+
+    for k in 1:max_iters
+        αₖ = .9*αₖ  # this is kinda funky, but it works
+
+        xₖ_prev = xₖ
+        xₖ = xₖ_prev + αₖ*(proj*∇f(xₖ_prev))
+        # println(xₖ)
+
+        if abs(f(xₖ)-f(xₖ_prev)) ≤ ϵ
+            break
+        end
+    end
+
+    return round.(xₖ,digits=3)
+end
+
+# fucking finally!!!!!!
+# grad = subgradient([.5,.5,.5])
+# grad'*proj
+
+# Γ = constraintSet([2.0,2.0,2.0],3.0,false)
+# lineSearch(Γ)
+
+gradientAscent([2.0,2.0,2.0],3.0,20)

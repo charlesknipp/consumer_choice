5a16de9fb89e8114f082c9dd0ae6684ea081f537
classicharlie <cknipp@asu.edu>
2021-04-20 13:34:59 -0700

    upload benchmark process

---
+++ benchmarks/benchmark_algos.jl
@@ -0,0 +1,25 @@
+using JuMP
+using Ipopt
+
+model = Model(Ipopt.Optimizer)
+@variable(model, x1 >= 0)
+@variable(model, x2 >= 0)
+@variable(model, x3 >= 0)
+@variable(model, x4 >= 0)
+@NLobjective(model, Max, (x1^.3)*(x2^.2)*(x3^.1)*(x4^.4))
+@constraint(model, Γ, 2x1 + 2x2 + 2x3 + 2x4 <= 3)
+
+@time optimize!(model)
+
+@show value(x1)
+@show value(x2)
+@show value(x3)
+@show value(x4)
+
+# 0.45
+# 0.3
+# 0.15
+# 0.6
+
+# test time      = .017 seconds
+# algorithm time = .069 seconds

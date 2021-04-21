c7fd61a03dfed33d2cd980770cbc25e5b75a8171
classicharlie <cknipp@asu.edu>
2021-04-20 22:45:14 -0700

    fixed SQP and specified array types

--- drafts/draft-4.jl
+++ drafts/draft-4.jl
@@ -1,12 +1,13 @@
 using Base.Iterators
 using LinearAlgebra
+using BenchmarkTools
 
 # just use Cobb-Douglas because it is supermodular in x on X = {x|x∈Rⁿ,α≥0}
-α = [.3,.2,.5]
-f(x) = reduce(*,x.^α)
-lagrange(x,λ) = f(x) + λ*(m-reduce(+,p.*x))
+α = [.3,.2,.5,.4]
+f(x::Vector{Float64}) = reduce(*,x.^α)
 
-function constraintSet(p::Array,t::Float64,equality::Bool=false,δ::Int=2)
+# almost as inefficient as the binary search itself, but works really well
+function constraintSet(p::Vector{Float64},t::Float64,equality::Bool=false,δ::Int64=2)
     feasible(x)  = sum(p.*x)-t ≤ 0 ? true : false
     efficient(x) = abs(sum(p.*x)-t) ≤ exp10(-δ) ? true : false
 
@@ -19,8 +20,8 @@ function constraintSet(p::Array,t::Float64,equality::Bool=false,δ::Int=2)
     return equality == true ? filter(efficient,space) : filter(feasible,space)
 end
 
-
-function binarySearch(p::Array,t::Float64,equality::Bool=true)
+# the literal worst algorithm epsecially in higher dimensions with dense spaces
+function binarySearch(p::Vector{Float64},t::Float64,equality::Bool=true)
     global x_optimal
     Γ = constraintSet(p,t,equality)
     x_optimal = Γ[1]
@@ -35,47 +36,117 @@ function binarySearch(p::Array,t::Float64,equality::Bool=true)
 end
 
 # derive using the limit definition
-function subgradient(x::Vector,step::Float64)
+function subgradient(x::Vector{Float64},ϵ::Float64)
     n = length(x)
     ∇f = zeros(n)
 
     for i = 1:n
-        x_prime = copy(x)
-        x_prime[i] += step
-        ∇f[i] = (f(x_prime)-f(x))/step
+        x_plus, x_minus = copy(x), copy(x)
+        x_plus[i]  += ϵ
+        x_minus[i] -= ϵ
+        ∇f[i] = (f(x_plus)-f(x_minus))/(2*ϵ)
     end
 
     return ∇f
 end
 
-function gradientAscent(p,t,δ,max_iters=1000)
+# there's an issue here with nonsingularity given small enough epsilon values
+function hessian(x::Vector{Float64},ϵ::Float64)
+    n = length(x)
+    H = zeros(n,n)
+    xpp,xpm,xmp,xmm = copy(x),copy(x),copy(x),copy(x)
+
+    for i = 1:n
+        xi = x[i]
+        xpp[i],xmm[i] = xi+ϵ,xi-ϵ
+
+        H[i,i] = (f(xpp)-2*f(x)+f(xmm))/(ϵ*ϵ)
+
+        xp = xi + ϵ
+        xm = xi - ϵ
+
+        xpp[i],xpm[i] = xp,xp
+        xmp[i],xmm[i] = xm,xm
+
+        for j = i+1:n
+            xj = x[j]
+            xp = xj + ϵ
+            xm = xj - ϵ
+
+            xpp[j],xpm[j] = xp,xm
+            xmp[j],xmm[j] = xp,xm
+
+            H[i,j] = (f(xpp)-f(xpm)-f(xmp)+f(xmm))/(4*ϵ*ϵ)
+
+            xpp[j],xpm[j] = xj,xj
+            xmp[j],xmm[j] = xj,xj
+        end
+
+        xpp[i],xpm[i] = xi,xi
+        xmp[i],xmm[i] = xi,xi
+    end
+
+    LinearAlgebra.copytri!(H,'U')
+end
+
+# performs a gradient ascent on the objective and projects onto the constraint
+function projectedGradientAscent(p::Vector{Float64},t::Float64,δ::Int64=8,max_iters::Int64=1000)
     n = length(p)
     ϵ = exp10(-δ)
 
     global xₖ = [t/(n*pᵢ) for pᵢ in p]
-    global αₖ = 1
+    global xₖ_prev
 
-    ∇f(x) = subgradient(x,.00000001)
+    ∇f(x) = subgradient(x,ϵ)
     proj  = Matrix(1.0I,n,n) - p*inv(p'*p)*p'
 
     for k in 1:max_iters
-        αₖ = .9*αₖ  # this is kinda funky, but it works
+        if k == 1
+            αₖ = 1
+        else
+            Δx, ΔDf = xₖ+xₖ_prev, ∇f(xₖ)+∇f(xₖ_prev)
+            αₖ = (Δx⋅ΔDf)/(ΔDf⋅ΔDf)
+        end
 
         xₖ_prev = xₖ
-        xₖ = xₖ_prev + αₖ*(proj*∇f(xₖ_prev))
+        term = αₖ*(proj*∇f(xₖ_prev))
+        xₖ = xₖ_prev + term
 
-        if abs(f(xₖ)-f(xₖ_prev)) ≤ ϵ
-            break
-        end
+        abs(f(xₖ)-f(xₖ_prev)) ≤ ϵ ? break : continue
     end
 
     return round.(xₖ,digits=3)
 end
 
-# for benchmarking and comparing computational efficiency
-function benchmark(algorithm::Function)
-    p_test = [2.0,2.0,2.0]
-    t_test = 3.0
+# this operates on the lagrangian rather than the objective function
+function SQP(p::Vector{Float64},t::Float64,δ::Int64=8,max_iters::Int64=1000)
+    n = length(p)
+    ϵ = exp10(-δ)
 
-    @time algorithm(p_test,t_test)
+    global xₖ = [t/(n*pᵢ) for pᵢ in p]
+    global λₖ = 1.0
+    global xₖ_prev
+
+    ∇f(x) = subgradient(x,ϵ)
+    Hf(x) = hessian(x,.001)
+
+    for k in 1:max_iters
+        xₖ_prev = xₖ
+        dk = [Hf(xₖ) -p; -p' 0.0]\[(-∇f(xₖ)-λₖ*p)' p'*xₖ-t]'
+        Δx,Δλ = dk[1:n],dk[n+1]
+        xₖ = abs.(xₖ+Δx)
+        λₖ = abs(λₖ-Δλ)
+
+        abs(f(xₖ)-f(xₖ_prev)) ≤ ϵ ? break : continue
+    end
+
+    return (round.(xₖ,digits=3),round(λₖ,digits=δ))
 end
+
+# for benchmarking and comparing computational efficiency
+p_test4 = [2.0,2.0,2.0,2.0]
+p_test3 = [2.0,2.0,2.0]
+t_test = 3.0
+
+@benchmark SQP(p_test4,t_test)
+# @benchmark projectedGradientAscent(p_test,t_test)

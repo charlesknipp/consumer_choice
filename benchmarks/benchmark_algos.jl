07eb1ad1f385eb81412f767ee881fe5546607001
classicharlie <cknipp@asu.edu>
2021-04-27 11:34:00 -0700

    benchmarked non-concave objective

--- benchmarks/benchmark_algos.jl
+++ benchmarks/benchmark_algos.jl
@@ -1,25 +1,34 @@
 using JuMP
 using Ipopt
 
-model = Model(with_optimizer(Ipopt.Optimizer))
-@variable(model, x1 >= 0)
-@variable(model, x2 >= 0)
-@variable(model, x3 >= 0)
-@variable(model, x4 >= 0)
-@NLobjective(model, Max, (x1^.3)*(x2^.2)*(x3^.1)*(x4^.4))
-@constraint(model, Γ, 2x1 + 2x2 + 2x3 + 2x4 <= 3)
+function case1()
+    model = Model(with_optimizer(Ipopt.Optimizer))
+    @variable(model, x1 >= 0)
+    @variable(model, x2 >= 0)
+    @variable(model, x3 >= 0)
+    @variable(model, x4 >= 0)
+    @NLobjective(model, Max, (x1^.3)*(x2^.2)*(x3^.1)*(x4^.4))
+    @constraint(model, Γ, 2x1 + 2x2 + 2x3 + 2x4 <= 3)
 
-@time optimize!(model)
+    optimize!(model)
 
-@show value(x1)
-@show value(x2)
-@show value(x3)
-@show value(x4)
+    @show value(x1)
+    @show value(x2)
+    @show value(x3)
+    @show value(x4)
+end
 
-# 0.45
-# 0.3
-# 0.15
-# 0.6
+function case2()
+    model = Model(with_optimizer(Ipopt.Optimizer))
+    @variable(model, x1 >= 0)
+    @variable(model, x2 >= 0)
 
-# test time      = .017 seconds
-# algorithm time = .069 seconds
+    @NLobjective(model, Max, x1*(x2-1.0)^3 + x1)
+    @constraint(model, Γ, x1 + x2 <= 2.93)
+
+    optimize!(model)
+
+    @show value(x1)
+    @show value(x2)
+    @show dual(Γ)
+end
